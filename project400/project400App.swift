d855f8f833c7a2502b80d80c84e45c05043febe7
s00190262 <s00190262@mail.itsligo.ie>
2022-12-08 03:04:58 +0000

    Initial Commit

---
+++ project400/project400App.swift
@@ -0,0 +1,17 @@
+//
+//  project400App.swift
+//  project400
+//
+//  Created by Omar Alshuaili on 08/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct project400App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
